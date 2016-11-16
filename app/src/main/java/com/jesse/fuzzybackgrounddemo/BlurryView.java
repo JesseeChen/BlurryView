1fc180df951af3bc8972f4b665fd087cc7219ce6
jesse <363780967@qq.com>
2016-11-16 11:22:56 +0800

    渲染成功

--- app/src/main/java/com/jesse/fuzzybackgrounddemo/BlurryView.java
+++ app/src/main/java/com/jesse/fuzzybackgrounddemo/BlurryView.java
@@ -1,7 +1,6 @@
 package com.jesse.fuzzybackgrounddemo;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
@@ -9,21 +8,18 @@ import android.renderscript.Allocation;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 import android.util.AttributeSet;
-import android.util.TypedValue;
+import android.util.Log;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
  * Created by Jesse on 2016/11/15.
  */
 public class BlurryView extends RelativeLayout {
-    private int mBitmapId;
-    private int imageWidth;
-    private int imageHeight;
+    private static final String TAG = BlurryView.class.getSimpleName();
     private ImageView mImageView;
+    private Bitmap mBitmap;
 
     public BlurryView(Context context) {
         super(context);
@@ -31,18 +27,31 @@ public class BlurryView extends RelativeLayout {
 
     public BlurryView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initRes(context, attrs);
-        mImageView = new ImageView(context);
-        LayoutParams layoutParams = new LayoutParams(imageWidth, imageHeight);
-        mImageView.setLayoutParams(layoutParams);
-        mImageView.setImageResource(mBitmapId);
-        mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        addView(mImageView);
     }
 
-    private void doBlurry(ImageView imageView) {
-        Bitmap bitmap = BitmapUtil.drawable2Bitmap(imageView.getDrawable());
-        Bitmap overlay = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+    private ImageView getChildImageView() {
+        return (ImageView) findViewById(R.id.image_id);
+    }
+
+    private void addViewObserver() {
+        mImageView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                mImageView.getViewTreeObserver().removeOnPreDrawListener(this);
+                mImageView.setDrawingCacheEnabled(true);
+                mBitmap = mImageView.getDrawingCache();
+
+                Bitmap overlay = doBlurry(mBitmap, 10, 0.5f);
+                setBackground(new BitmapDrawable(getResources(), overlay));
+                mImageView.setDrawingCacheEnabled(false);
+                return true;
+            }
+        });
+    }
+
+    private Bitmap doBlurry(Bitmap bitmap, int blurryRadius, float ratio) {
+        long startTime = System.currentTimeMillis();
+        Bitmap overlay = Bitmap.createBitmap((int) (bitmap.getWidth() * ratio), (int) (bitmap.getHeight() * ratio), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(overlay);
         canvas.drawBitmap(bitmap, 0, 0, null);
 
@@ -50,37 +59,22 @@ public class BlurryView extends RelativeLayout {
         RenderScript rs = RenderScript.create(getContext());
         Allocation allocation = Allocation.createFromBitmap(rs, overlay);
         ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, allocation.getElement());
-
         //渲染
         blur.setInput(allocation);
-        blur.setRadius(20);
+        blur.setRadius(blurryRadius);
         blur.forEach(allocation);
         allocation.copyTo(overlay);
-
-        setBackground(new BitmapDrawable(getResources(), overlay));
-
         //回收
         rs.destroy();
+        Log.d(TAG, "doBlurry: during " + (System.currentTimeMillis() - startTime) + " ms");
+        return overlay;
     }
 
-    private void initRes(Context context, AttributeSet attrs) {
-        TypedArray typedArray = getResources().obtainAttributes(attrs, R.styleable.BlurryView);
-        mBitmapId = typedArray.getResourceId(R.styleable.BlurryView_src, 0);
-        imageWidth = (int) typedArray.getDimension(R.styleable.BlurryView_image_width, 0);
-        imageHeight = (int) typedArray.getDimension(R.styleable.BlurryView_image_height, 0);
-
-        if (imageWidth != WRAP_CONTENT && imageWidth != MATCH_PARENT) {
-            imageWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, imageWidth, getResources().getDisplayMetrics());
-        }
-        if (imageHeight != WRAP_CONTENT && imageHeight != MATCH_PARENT) {
-            imageHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, imageHeight, getResources().getDisplayMetrics());
-        }
-        typedArray.recycle();
-    }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        doBlurry(mImageView);
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mImageView = getChildImageView();
+        addViewObserver();
     }
 }
